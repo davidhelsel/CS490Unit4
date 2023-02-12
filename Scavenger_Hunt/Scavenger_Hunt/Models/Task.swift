8c361c66c2afef20bce68eb44a6876e408067242
davidhelsel <112271678+davidhelsel@users.noreply.github.com>
2023-02-11 22:10:45 -0500

    uploaded files

---
+++ Scavenger_Hunt/Scavenger_Hunt/Models/Task.swift
@@ -0,0 +1,44 @@
+//
+//  Task.swift
+//  lab-task-squirrel
+//
+//  Created by Charlie Hieger on 11/15/22.
+//
+
+import UIKit
+import CoreLocation
+
+class Task {
+    let title: String
+    let description: String
+    var image: UIImage?
+    var imageLocation: CLLocation?
+    var isComplete: Bool {
+        image != nil
+    }
+
+    init(title: String, description: String) {
+        self.title = title
+        self.description = description
+    }
+
+    func set(_ image: UIImage, with location: CLLocation) {
+        self.image = image
+        self.imageLocation = location
+    }
+}
+
+extension Task {
+    static var mockedTasks: [Task] {
+        return [
+            Task(title: "Your favorite local restauraunt",
+                 description: "Where is your favorite place to eat?"),
+            Task(title: "Your favorite local cafe",
+                 description: "Where is your favorite place to go for coffee?"),
+            Task(title: "You go-to brunch place",
+                 description: "Where do you go"),
+            Task(title: "Your favorite hiking spot",
+                description: "Where do you go to be one with nature?")
+        ]
+    }
+}
